b5a969cf1422274a4341eb4330daefe1bf3953ee
twulfz <andyvelezespejo@gmail.com>
2024-05-08 17:40:53 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/twulfz/ecualert/Activity_inicio_sesion.java
@@ -0,0 +1,41 @@
+package com.twulfz.ecualert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_inicio_sesion extends AppCompatActivity {
+
+    Button btnLoggin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio_sesion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnLoggin = findViewById(R.id.btnLoggin);
+
+        btnLoggin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // send to main activity
+                Intent intent = new Intent(Activity_inicio_sesion.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
